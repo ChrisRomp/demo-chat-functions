98eee5693027f29dbc4f3fe1897941428c1e0b6f
Chris Romp <ChrisRomp@users.noreply.github.com>
2023-11-16 19:17:12 +0000

    Updates for app service publishing.

--- src/webApp/app.py
+++ src/webApp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 import asyncio
+import os
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ async def get_response():
 
 async def call_api(prompt):
     # Replace 'api_endpoint' and 'api_key' with actual API details
-    api_endpoint = "http://localhost:7071/api/http_trigger"
+    api_endpoint = os.getenv('API_ENDPOINT', "http://localhost:7071/api/http_trigger")
     headers = {"Content-Type": "application/json"}
     data = {"name": prompt}
 
@@ -30,5 +31,10 @@ async def call_api(prompt):
     response = await loop.run_in_executor(None, make_request)
     return response.text
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Running in debug mode
+# if __name__ == '__main__':
+#     HOST = os.getenv('HOST', '0.0.0.0')
+#     try:
+#         PORT = int(os.getenv('PORT', '8000'))
+#     except ValueError:
+#         PORT = 8000
+#     app.run(host=HOST, port=PORT)
